5e516cfa5912ae1759da0c451d17bb0ee56fe0fb
Sean OReilly <soreilly424@gmail.com>
2021-12-14 13:20:54 -0500

    added motion

---
+++ components/Jumbotron.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import Image from 'next/image'
+import { urlFor } from '../lib/client'
+import { motion } from 'framer-motion'
+
+function Jumbotron({ banner }) {
+  const { image, subtitle } = banner
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { ease: 'easeOut', duration: 2, delay: 0.5 },
+    },
+  }
+
+  const header1 = {
+    hidden: { x: 300, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { ease: 'easeInOut', duration: 0.9 },
+    },
+  }
+
+  const header2 = {
+    hidden: { x: -300, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { ease: 'easeInOut', duration: 0.9 },
+    },
+  }
+
+  const button1 = {
+    hidden: { y: 400 },
+    show: { y: 0, transition: { duration: 1.2 } },
+  }
+  const button2 = {
+    hidden: { y: 400 },
+    show: { y: 0, transition: { duration: 1.5 } },
+  }
+
+  return (
+    <div className="relative h-screen">
+      <Image className="object-cover" src={urlFor(image).url()} layout="fill" />
+      <div className="absolute flex flex-col w-full h-full items-center justify-center text-center">
+        <div className="max-w-7xl inline-flex flex-col gap-7 mt-64">
+          <motion.div
+            initial="hidden"
+            animate="show"
+            variants={container}
+            className="flex gap-2 items-center text-gray-100 text-7xl font-bold"
+          >
+            <motion.h1 variants={header1}>Taylor Lawn</motion.h1>
+            <h1>&</h1>
+            <motion.h1 variants={header2}>Landscaping</motion.h1>
+          </motion.div>
+
+          <h4 className="text-5xl font-bold text-gray-100 ">{subtitle}</h4>
+          <div className="flex gap-3 justify-center">
+            <motion.button
+              initial="hidden"
+              animate="show"
+              variants={button1}
+              className="rounded-full text-lg px-8 py-3 text-white border border-white"
+            >
+              About us
+            </motion.button>
+            <motion.button
+              initial="hidden"
+              animate="show"
+              variants={button2}
+              className="text-green-600 text-lg bg-white rounded-full px-8 py-3"
+            >
+              Request an Estimate
+            </motion.button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Jumbotron
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head'
 import { getBanner } from '../lib/api'
-import { urlFor } from '../lib/client'
-import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Jumbotron from '../components/Jumbotron'
 
 export default function Home({ banner }) {
-  console.log(banner)
-  const { image, subtitle, title } = banner[0]
-
   return (
     <div>
       <Head>
@@ -15,27 +11,7 @@ export default function Home({ banner }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div className="relative h-screen">
-        <Image
-          className="object-cover"
-          src={urlFor(image).url()}
-          layout="fill"
-        />
-        <div className="absolute flex flex-col w-full h-full items-center justify-center text-center">
-          <div className="max-w-7xl inline-flex flex-col gap-7 mt-64">
-            <h1 className="text-gray-100 text-7xl font-bold ">{title}</h1>
-            <h4 className="text-5xl font-bold text-gray-100 ">{subtitle}</h4>
-            <div className="flex gap-3 justify-center">
-              <button className="rounded-full text-lg px-8 py-3 text-white border border-white">
-                About us
-              </button>
-              <button className="text-green-600 text-lg bg-white rounded-full px-8 py-3">
-                Request an Estimate
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Jumbotron banner={banner} />
     </div>
   )
 }
@@ -43,7 +19,7 @@ export default function Home({ banner }) {
 export async function getStaticProps({ preview = false }) {
   const banner = await getBanner(preview)
   return {
-    props: { banner },
+    props: { banner: banner[0] },
     revalidate: 1,
   }
 }
