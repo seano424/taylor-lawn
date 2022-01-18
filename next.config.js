b6d97a0b9e9407b58380f9e64c6f88148d5c448d
Sean OReilly <soreilly424@gmail.com>
2022-01-18 12:56:38 -0600

    updated next config and package json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-// cdn.sanity.io
-// next.config.js
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
 module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
   images: {
     domains: ['cdn.sanity.io'],
   },
-}
+};
