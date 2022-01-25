ab47dc7de9c9b390bf24211034e6a99b0668d5cf
Sean OReilly <soreilly424@gmail.com>
2022-01-24 20:45:16 -0600

    removed debug screens

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="debug-screens">
+        <body>
           <Main />
           <NextScript />
         </body>
