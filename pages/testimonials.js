20fc073596f44293afbbbad560c254914eb46cd7
Sean OReilly <soreilly424@gmail.com>
2022-03-02 17:03:11 -0600

    fixed layout for testimonials

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -1,17 +1,8 @@
 import { getTestimonials } from '../lib/api'
-import Layout from '../components/Layout'
 import Testimonials from '../components/pages/Testimonials'
-import Footer from '../components/Footer'
 
 export default function Home({ testimonials }) {
-  return (
-    <Layout>
-      <div className="relative lg:top-40">
-        <Testimonials content={testimonials} />
-        <Footer />
-      </div>
-    </Layout>
-  )
+  return <Testimonials content={testimonials} />
 }
 
 export async function getStaticProps({ preview = false }) {
