d9688f13689c008e1a4039527fa47bfa94966b03
Sean OReilly <soreilly424@gmail.com>
2022-03-02 19:44:21 -0600

    Fixed services page

--- pages/services.js
+++ pages/services.js
@@ -1,14 +1,5 @@
-import Layout from '../components/Layout'
-import Footer from '../components/Footer'
 import Services from '../components/pages/Services'
 
 export default function Home() {
-  return (
-    <Layout>
-      <div className="relative lg:top-40">
-        <Services />
-        <Footer />
-      </div>
-    </Layout>
-  )
+  return <Services />
 }
