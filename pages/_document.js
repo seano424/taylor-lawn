b33202b6b66836d3fd5b1c4314eeef48ce079706
Sean OReilly <soreilly424@gmail.com>
2022-08-09 11:03:42 -0500

    Language Added To HTML

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
-      <Head lang='en' />
+    <Html lang='en'>
+      <Head />
       <body>
         <Main />
         <NextScript />
