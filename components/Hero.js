f1459fa9bdd23d1cb2f2e78c06ea9446a82cb2af
Sean OReilly <soreilly424@gmail.com>
2022-08-30 13:56:27 -0500

    Updated hero text position

--- components/Hero.js
+++ components/Hero.js
@@ -13,18 +13,20 @@ function Hero() {
         sizes="(min-width: 75em) 100vw,
             90vw"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center gap-5 text-center">
-        <h1 className="h1">Taylor Lawn & Landscaping L.L.C.</h1>
-        <h4 className="h4">The True Green Thumb</h4>
-        <div className="mx-10 flex flex-col justify-center gap-3 md:flex-row">
-          <Link href="/contact">
-            <a className="button">Contact us</a>
-          </Link>
-          <Link href="/contact">
-            <a className="button bg-white text-emerald-700 transition duration-300 hover:border-emerald-700 hover:bg-emerald-700 hover:text-white">
-              Request an Estimate
-            </a>
-          </Link>
+      <div className="absolute inset-0">
+        <div className="flex h-full flex-col items-center justify-center gap-5 text-center">
+          <h1 className="h1">Taylor Lawn & Landscaping L.L.C.</h1>
+          <h4 className="h4">The True Green Thumb</h4>
+          <div className="mx-10 flex flex-col justify-center gap-3 md:flex-row">
+            <Link href="/contact">
+              <a className="button">Contact us</a>
+            </Link>
+            <Link href="/contact">
+              <a className="button bg-white text-emerald-700 transition duration-300 hover:border-emerald-700 hover:bg-emerald-700 hover:text-white">
+                Request an Estimate
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
