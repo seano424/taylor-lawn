8044222a8eb7f87654ddc33d19a04e281befe917
Sean OReilly <soreilly424@gmail.com>
2022-08-30 14:53:45 -0500

    Updating a's to Links

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Link from 'next/link'
 import {
   FaFacebook,
   FaClock,
@@ -47,12 +48,12 @@ function Header() {
                     <p>Sat: 10:30-2:00</p>
                   </div>
                 </div>
-                <div className="flex items-center gap-2 font-bold text-black">
-                  <FaMailBulk className="h-6" />
-                  <a href="mailto:taylorlawn@hotmail.com?subject=Hello Taylor Lawn and Landscaping!">
+                <Link href="mailto:taylorlawn@hotmail.com?subject=Hello Taylor Lawn and Landscaping!">
+                  <a className="flex items-center gap-2 font-bold text-black">
+                    <FaMailBulk className="h-6" />
                     taylorlawn@hotmail.com
                   </a>
-                </div>
+                </Link>
                 <div className="flex items-center gap-2">
                   <FaSearchLocation className="h-6" />
                   <span className="22xl:block hidden font-black">
@@ -60,75 +61,76 @@ function Header() {
                   </span>
                   <span className="22xl:hidden font-black">KCMO</span>
                 </div>
-                <a
-                  rel="noopener"
-                  className="flex gap-2"
-                  target="_blank"
-                  href="https://www.facebook.com/TaylorLawnandLandscaping"
-                >
-                  <FaFacebook className="h-6 w-6" />
-                </a>
+                <Link href="https://www.facebook.com/TaylorLawnandLandscaping">
+                  <a rel="noopener" className="flex gap-2" target="_blank">
+                    <FaFacebook className="h-6 w-6" />
+                  </a>
+                </Link>
               </div>
             </div>
             <nav className="relative flex items-center pt-6 text-lg text-gray-800">
               <ul className="hidden flex-1 items-center justify-between gap-8 font-serif lg:flex">
-                <a
-                  href="/"
-                  className={`${
-                    route === '/' ? 'font-black text-emerald-500' : ''
-                  } hover:text-emerald-500`}
-                >
-                  Home
-                </a>
-                <a
-                  href="/about"
-                  className={`${
-                    route === '/about' ? 'font-black text-emerald-500' : ''
-                  } hover:text-emerald-500`}
-                >
-                  About
-                </a>
-                <a
-                  href="/services"
-                  className={`${
-                    route === '/services' ? 'font-black text-emerald-500' : ''
-                  } hover:text-emerald-500`}
-                >
-                  Service
-                </a>
-                <a
-                  href="/gallery"
-                  className={`${
-                    route === '/gallery' ? 'font-black text-emerald-500' : ''
-                  } hover:text-emerald-500`}
-                >
-                  Gallery
-                </a>
-                <a
-                  href="/testimonials"
-                  className={`${
-                    route === '/testimonials'
-                      ? 'font-black text-emerald-500'
-                      : ''
-                  } hover:text-emerald-500`}
-                >
-                  Testimonials
-                </a>
-                <a
-                  href="/contact"
-                  className={`${
-                    route === '/contact' ? 'font-black text-emerald-500' : ''
-                  } hover:text-emerald-500`}
-                >
-                  Contact
-                </a>
-
-                <button
-                  onClick={() => push('/contact')}
-                  className="rounded-full bg-emerald-500 py-2 px-6 text-[15px] font-thin tracking-wide text-white"
-                >
-                  Free Quote
-                </button>
+                <Link href="/">
+                  <a
+                    className={`${
+                      route === '/' ? 'font-black text-emerald-500' : ''
+                    } hover:text-emerald-500`}
+                  >
+                    Home
+                  </a>
+                </Link>
+                <Link href="/about">
+                  <a
+                    className={`${
+                      route === '/about' ? 'font-black text-emerald-500' : ''
+                    } hover:text-emerald-500`}
+                  >
+                    About
+                  </a>
+                </Link>
+                <Link href="/services">
+                  <a
+                    className={`${
+                      route === '/services' ? 'font-black text-emerald-500' : ''
+                    } hover:text-emerald-500`}
+                  >
+                    Service
+                  </a>
+                </Link>
+                <Link href="/gallery">
+                  <a
+                    className={`${
+                      route === '/gallery' ? 'font-black text-emerald-500' : ''
+                    } hover:text-emerald-500`}
+                  >
+                    Gallery
+                  </a>
+                </Link>
+                <Link href="/testimonials">
+                  <a
+                    className={`${
+                      route === '/testimonials'
+                        ? 'font-black text-emerald-500'
+                        : ''
+                    } hover:text-emerald-500`}
+                  >
+                    Testimonials
+                  </a>
+                </Link>
+                <Link href="/contact">
+                  <a
+                    className={`${
+                      route === '/contact' ? 'font-black text-emerald-500' : ''
+                    } hover:text-emerald-500`}
+                  >
+                    Contact
+                  </a>
+                </Link>
+                <Link href="/contact">
+                  <a className="rounded-full bg-emerald-500 py-2 px-6 text-[15px] font-thin tracking-wide text-white">
+                    Free Quote
+                  </a>
+                </Link>
               </ul>
             </nav>
           </section>
