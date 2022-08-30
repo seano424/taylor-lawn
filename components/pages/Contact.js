feb468d5ca4a2f16c341d270e223a516a2790c9f
Sean OReilly <soreilly424@gmail.com>
2022-08-30 16:55:15 -0500

    Contact Padding And Margi

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -12,49 +12,47 @@ function Contact() {
   return (
     <>
       <Modal open={open} setOpen={setOpen} />
-      <section className="py-base grid gap-10 bg-gray-50/25 lg:grid-cols-2 xl:px-36">
-        <article className="text-emerald-700">
-          <div className="flex flex-col items-center gap-4 text-3xl font-black xl:text-5xl">
-            <h2>Contact details</h2>
-            <p className="text-base font-normal text-gray-600">
-              TXT or CALL for a free estimate today
-            </p>
-            <a className="font-serif" href="tel:+1-913-206-7214">
+      <section className="py-base grid gap-10 bg-gray-50/25 text-emerald-700 lg:grid-cols-2 xl:px-36">
+        <div className="flex flex-col items-center gap-4 text-3xl font-black xl:text-5xl">
+          <h2>Contact details</h2>
+          <p className="text-base font-normal text-gray-600">
+            TXT or CALL for a free estimate today
+          </p>
+          <a className="font-serif" href="tel:+1-913-206-7214">
+            (913) 206 7214
+          </a>
+        </div>
+        <div className="my-5 mx-6 grid items-center gap-4 text-center text-lg sm:grid-cols-2 md:my-10 lg:m-12 lg:gap-10 lg:text-left">
+          <div className="flex flex-col items-center">
+            <FaSearchLocation className="w-6 md:w-12" />
+            <p>Location</p>
+            <p className="text-gray-600">Merriam, Kansas</p>
+          </div>
+          <div className=" flex flex-col items-center">
+            <FaPhone className="w-6 md:w-12" />
+            <p>Call Us</p>
+            <a className="text-gray-600" href="tel:+1-913-206-7214">
               (913) 206 7214
             </a>
           </div>
-          <div className="my-10 mx-6 grid items-center gap-4 text-center text-lg md:grid-cols-2 lg:m-12 lg:gap-10 lg:text-left">
-            <div className="flex flex-col items-center">
-              <FaSearchLocation className="w-6 md:w-12" />
-              <p>Location</p>
-              <p className="text-gray-600">Merriam, Kansas</p>
-            </div>
-            <div className=" flex flex-col items-center">
-              <FaPhone className="w-6 md:w-12" />
-              <p>Call Us</p>
-              <a className="text-gray-600" href="tel:+1-913-206-7214">
-                (913) 206 7214
-              </a>
-            </div>
-            <div className="flex flex-col items-center">
-              <FaMailBulk className="w-6 md:w-12" />
-              <a
-                className="text-gray-600"
-                href="mailto:taylorlawn@hotmail.com?subject=Hello Taylor Lawn and Landscaping!"
-              >
-                Write to Us
-              </a>
-            </div>
-            <div className="flex flex-col items-center gap-1">
-              <FaClock className="w-6 md:w-12" />
-              <p>Working Hours</p>
-              <p className="text-gray-600">Monday - Friday:</p>
-              <span>8:00-5:00</span>
-              <p className="text-gray-600">Saturday:</p>
-              <span>10:30-2:00</span>
-            </div>
+          <div className="hidden flex-col items-center sm:flex">
+            <FaMailBulk className="w-6 md:w-12" />
+            <a
+              className="text-gray-600"
+              href="mailto:taylorlawn@hotmail.com?subject=Hello Taylor Lawn and Landscaping!"
+            >
+              Write to Us
+            </a>
           </div>
-        </article>
+          <div className="flex flex-col items-center gap-1">
+            <FaClock className="w-6 md:w-12" />
+            <p>Working Hours</p>
+            <p className="text-gray-600">Monday - Friday:</p>
+            <span>8:00-5:00</span>
+            <p className="text-gray-600">Saturday:</p>
+            <span>10:30-2:00</span>
+          </div>
+        </div>
         <Formik
           initialValues={{ name: '', email: '', subject: '', message: '' }}
           validationSchema={Yup.object({
@@ -88,8 +86,8 @@ function Contact() {
             setValue('')
           }}
         >
-          <Form className="mx-auto flex max-w-sm flex-col gap-4  px-3 sm:mx-0 sm:max-w-full">
-            <h2 className="mb-4 text-5xl font-black text-emerald-700">
+          <Form className="container mx-auto flex max-w-sm flex-col gap-4 px-3 sm:max-w-lg lg:mx-0 lg:max-w-full">
+            <h2 className="mb-4 text-center text-5xl font-black text-emerald-700 sm:text-left">
               Have a question?
             </h2>
             <label className="font-thin text-gray-900" htmlFor="name">
