77a1c2819bfae272a113fe4d01bcce3e12ceee3e
Sean OReilly <soreilly424@gmail.com>
2022-08-31 14:11:02 -0500

    Updated Hero Image Size And button position

--- components/Hero.js
+++ components/Hero.js
@@ -4,11 +4,11 @@ import { imageBuilder } from 'lib/client'
 
 function Hero({ content }) {
   return (
-    <div className="relative min-h-[800px] xl:min-h-[730px]">
+    <div className="relative min-h-[840px] sm:min-h-[950px] xl:min-h-[775px]">
       <Image
         className="min-h-[1200px] w-full object-cover"
         alt="Hero Image"
-        src={imageBuilder(content.image).width(1680).height(730).url()}
+        src={imageBuilder(content.image).width(1680).height(775).url()}
         layout="fill"
         priority
       />
@@ -16,7 +16,7 @@ function Hero({ content }) {
         <div className="flex h-full flex-col items-center justify-center gap-5 text-center">
           <h1 className="h1">{content.title}</h1>
           <p className="h4">{content.subtitle}</p>
-          <div className="mx-10 flex flex-col justify-center gap-3 md:flex-row">
+          <div className="m-5 flex flex-col justify-center gap-3 md:flex-row lg:m-10">
             <Link href={content.cta.url ?? '/contact'}>
               <a className="button 5 transform bg-white text-emerald-700 shadow-2xl transition duration-300 hover:bg-emerald-700 hover:text-white">
                 {content.cta.title ?? 'Request an Estimate Today!'}
