76b6de5fddf1b3a4869c18303a255f614d32d692
Sean OReilly <soreilly424@gmail.com>
2022-09-05 10:35:42 -0500

    Removed mounted state

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,14 @@
-import { useEffect, useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Meta from './Meta'
 
 function Layout({ children }) {
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => {
-    setMounted(true)
-  }, [])
   return (
     <div>
       <Meta />
       <Header />
-      {mounted && <Sidebar />}
+      <Sidebar />
       <main>{children}</main>
       <Footer />
     </div>
