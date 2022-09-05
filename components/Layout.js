33cde18a7cf33ecff865eefb112b5d8abf938ae8
Sean OReilly <soreilly424@gmail.com>
2022-09-05 10:16:47 -0500

    Sidebar Will Load After Mounting Page

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,19 @@
+import { useEffect, useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Meta from './Meta'
 
 function Layout({ children }) {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
   return (
     <div>
       <Meta />
       <Header />
-      <Sidebar />
+      {mounted && <Sidebar />}
       <main>{children}</main>
       <Footer />
     </div>
