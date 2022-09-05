7e9bfa0e96f6c8bdb7d70cad49272a69f2bfbba2
Sean OReilly <soreilly424@gmail.com>
2022-09-05 10:52:32 -0500

    Router added back to navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,7 @@
 import { m } from 'framer-motion'
 import MenuItem from './MenuItem'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const variants = {
   open: {
@@ -29,18 +30,23 @@ const header = {
 }
 
 const Navigation = ({ open, toggle }) => {
+  const router = useRouter()
+  const handleHeader = () => {
+    router.push('/')
+    toggle()
+  }
+
   return (
     <m.ul className={`menu-ul ${!open && 'hidden'}`} variants={variants}>
-      <Link href="/" passHref>
-        <m.a
-          variants={header}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          className="flex-1 cursor-pointer flex-wrap rounded-md pb-4 font-sans text-xl font-black uppercase tracking-widest text-sky-900 hover:text-emerald-500"
-        >
-          Taylor Lawn & Landscaping L.L.C
-        </m.a>
-      </Link>
+      <m.h1
+        onClick={handleHeader}
+        variants={header}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        className="flex-1 cursor-pointer flex-wrap rounded-md pb-4 font-sans text-xl font-black uppercase tracking-widest text-sky-900 hover:text-emerald-500"
+      >
+        Taylor Lawn & Landscaping L.L.C
+      </m.h1>
       {menuItems.map((item, idx) => (
         <MenuItem toggle={toggle} i={idx} text={item} key={idx} />
       ))}
